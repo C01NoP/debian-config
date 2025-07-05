f2368a15ad2622621e69890c2d87f63057b714c1
mike <mike@debian>
2025-07-05 13:36:59 -0500

    update sid.sh script

--- sid.sh
+++ sid.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 
-set -e
+# Step 18: Change /etc/apt/sources.list to point to sid instead of bookworm
+sudo sed -i 's|/bookworm|/sid|g' /etc/apt/sources.list
 
-# Make sure to run as root
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run as root (e.g., sudo $0)"
-  exit 1
-fi
+# Step 19: Comment out lines that do NOT contain 'sid'
+sudo awk 'NR <= 5 {print; next} {if ($0 ~ /sid/) {print} else {print "#" $0}}' /etc/apt/sources.list | sudo tee /etc/apt/sources.list.new
+sudo mv /etc/apt/sources.list.new /etc/apt/sources.list
 
-# 1. Change /etc/apt/sources.list to point to sid
-sed -i 's/bookworm/sid/g' /etc/apt/sources.list
+# Run update and upgrade
+sudo apt update && sudo apt upgrade -y
 
-# 2. Comment out all lines except the first two
-awk 'NR<=2 {print; next} {print "#" $0}' /etc/apt/sources.list > /etc/apt/sources.list.tmp
-mv /etc/apt/sources.list.tmp /etc/apt/sources.list
+# Step 20: Add 'fish' to the bottom of ~/.bashrc
+echo "fish" >> ~/.bashrc
 
-# 3. Update and upgrade
-apt update
-apt upgrade -y
+# Step 21: Copy config.fish from /home/mike/Git/debian-config to ~/.config/fish/
+mkdir -p ~/.config/fish
+cp /home/mike/Git/debian-config/config.fish ~/.config/fish/
 
-# 4. Add 'fish' to the bottom of ~/.bashrc for user 'mike'
-echo "fish" >> /home/mike/.bashrc
-
-# 5. Copy config.fish from /home/mike/Git/debian-config to ~/.config/fish/
-# Ensure the target directory exists
-mkdir -p /home/mike/.config/fish
-cp /home/mike/Git/debian-config/config.fish /home/mike/.config/fish/
-
-# Change ownership to 'mike'
-chown -R mike:mike /home/mike/.config/fish
-chown mike:mike /home/mike/.bashrc
-
-# 6. Run autoremove and clean
-apt autoremove -y
-apt clean
-
-# 7. Reboot
-echo "All done. Rebooting now..."
-reboot
+# Step 22: Run autoremove and clean all
+sudo apt autoremove -y
+sudo apt clean
