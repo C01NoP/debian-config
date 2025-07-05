f6fc1a1e654a2c7bd953ee6b1b159b7c4468335a
C01NoP <37028464+C01NoP@users.noreply.github.com>
2025-07-05 18:32:28 -0500

    update stablesetup.sh

--- stablesetup.sh
+++ stablesetup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
 set -e
 
-# 1. Install sudo as root (assuming script runs as root)
+# 1. Install sudo as root
 apt update
 apt install -y sudo
 
@@ -12,56 +13,64 @@ usermod -aG sudo mike
 # 3. Install git, wget, vim
 apt install -y git wget vim
 
-# 4. Update /etc/apt/sources.list to include contrib and non-free
+# 4. Update /etc/apt/sources.list to include contrib and non-free repositories
 sed -i '/^deb / s/$/ contrib non-free/' /etc/apt/sources.list
 sed -i '/^deb-src / s/$/ contrib non-free/' /etc/apt/sources.list
 
 # 5. Add 32-bit architecture
 dpkg --add-architecture i386
 
-# 6. Add the DRD repo key
+# 6. Add DRD repo
 wget -O /etc/apt/trusted.gpg.d/drdteam.gpg https://debian.drdteam.org/drdteam.gpg
-
-# 7. Add DRD repo to sources.list
-echo "deb https://debian.drdteam.org/ stable multiverse" | tee -a /etc/apt/sources.list
+add-apt-repository 'deb https://debian.drdteam.org/ stable multiverse'
 
 # Update package lists
 apt update
 
-# 8. Install packages
+# 7. Install packages
 apt install -y gnome-shell gnome-tweaks nautilus vim fish steam-installer \
- mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 \
- libgl1-mesa-dri:i386 kitty vlc crispy-doom dsda-doom gzdoom gnome-calculator \
- timeshift deja-dup flatpak ffmpegthumbnailer curl dkms linux-headers-amd64 apt-listbugs \
- firefox-esr
+mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 \
+kitty vlc crispy-doom dsda-doom gzdoom gnome-calculator timeshift deja-dup flatpak \
+ffmpegthumbnailer curl dkms linux-headers-amd64
 
-# 9. Activate Flatpak
+# 8. Activate flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# 10. Clone Tela-icon-theme repo
-sudo -u mike git clone https://github.com/vinceliuice/Tela-icon-theme.git /home/mike/Git/Tela-icon-theme
+# 9. Clone Tela-icon-theme repository and run install.sh
+git clone https://github.com/vinceliuice/Tela-icon-theme.git /home/mike/Git/Tela-icon-theme
+chown -R mike:mike /home/mike/Git/Tela-icon-theme
+sudo -u mike bash -c "/home/mike/Git/Tela-icon-theme/install.sh"
 
-# 11. Run install.sh in Tela-icon-theme
-sudo -u mike bash /home/mike/Git/Tela-icon-theme/install.sh
+# 10. Clone Orchis-theme repository and run install.sh
+git clone https://github.com/vinceliuice/Orchis-theme.git /home/mike/Git/Orchis-theme
+chown -R mike:mike /home/mike/Git/Orchis-theme
+sudo -u mike bash -c "/home/mike/Git/Orchis-theme/install.sh"
 
-# 12. Clone Orchis-theme repo
-sudo -u mike git clone https://github.com/vinceliuice/Orchis-theme.git /home/mike/Git/Orchis-theme
+# 11. Clone Qogir-icon-theme repository and run install.sh
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git /home/mike/Git/Qogir-icon-theme
+chown -R mike:mike /home/mike/Git/Qogir-icon-theme
+sudo -u mike bash -c "/home/mike/Git/Qogir-icon-theme/install.sh"
 
-# 13. Run install.sh in Orchis-theme
-sudo -u mike bash /home/mike/Git/Orchis-theme/install.sh
+# 12. Append 'fish' to ~/.bashrc
+echo "fish" >> /home/mike/.bashrc
+chown mike:mike /home/mike/.bashrc
 
-# 14. Clone Qogir-icon-theme repo
-sudo -u mike git clone https://github.com/vinceliuice/Qogir-icon-theme.git /home/mike/Git/Qogir-icon-theme
+# 13. Change default shell to fish for user 'mike'
+chsh -s /usr/bin/fish mike
 
-# 15. Run install.sh in Qogir-icon-theme
-sudo -u mike bash /home/mike/Git/Qogir-icon-theme/install.sh
+# 14. Install fisher and bobthefish theme in fish shell
+sudo -u mike fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source; and fisher install jorgebucaran/fisher'
+sudo -u mike fish -c 'fisher install oh-my-fish/theme-bobthefish'
 
-# 16. Install Fisher
-sudo -u mike bash -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
+# 15. Create /var/lib/AccountsService/users/mike with specified content
+cat <<EOF | sudo tee /var/lib/AccountsService/users/mike
+[User]
+Language=
+XSession=gnome
+SystemAccount=true
+EOF
 
-# 17. Install bobthefish theme
-# To run fish commands, ensure fish shell is installed and set as default or run via fish -c
-apt install -y fish
-sudo -u mike fish -c "fisher install oh-my-fish/theme-bobthefish"
+# Change ownership
+sudo chown root:root /var/lib/AccountsService/users/mike
 
-echo "Setup completed successfully!"
+echo "Setup completed successfully."
