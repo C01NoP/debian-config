00778a9a18b2f0f715f96602b5c68b22017af4a9
mike <mike@debian>
2025-07-05 14:27:23 -0500

    update stablesetup.sh script

--- stablesetup.sh
+++ stablesetup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Exit immediately if a command exits with a non-zero status
 set -e
 
-# 1. Install sudo as root (assuming script is run as root)
+# 1. Install sudo as root (assuming script runs as root)
 apt update
 apt install -y sudo
 
@@ -14,53 +13,54 @@ usermod -aG sudo mike
 apt install -y git wget vim
 
 # 4. Update /etc/apt/sources.list to include contrib and non-free
-# Backup original sources.list
-cp /etc/apt/sources.list /etc/apt/sources.list.bak
-
-# Append 'contrib' and 'non-free' to each 'deb' and 'deb-src' line
 sed -i '/^deb / s/$/ contrib non-free/' /etc/apt/sources.list
 sed -i '/^deb-src / s/$/ contrib non-free/' /etc/apt/sources.list
 
-apt update
-
 # 5. Add 32-bit architecture
 dpkg --add-architecture i386
-apt update
 
-# 6. Add DRD repo: import GPG key and add repository
+# 6. Add the DRD repo key
 wget -O /etc/apt/trusted.gpg.d/drdteam.gpg https://debian.drdteam.org/drdteam.gpg
-add-apt-repository 'deb https://debian.drdteam.org/ stable multiverse'
 
+# 7. Add DRD repo to sources.list
+echo "deb https://debian.drdteam.org/ stable multiverse" | tee -a /etc/apt/sources.list
+
+# Update package lists
 apt update
 
-# 7. Install the specified packages
+# 8. Install packages
 apt install -y gnome-shell gnome-tweaks nautilus vim fish steam-installer \
-  mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 \
-  libgl1-mesa-dri:i386 kitty vlc crispy-doom dsda-doom gzdoom gnome-calculator \
-  timeshift deja-dup flatpak ffmpegthumbnailer curl dkms linux-headers-amd64
+ mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 \
+ libgl1-mesa-dri:i386 kitty vlc crispy-doom dsda-doom gzdoom gnome-calculator \
+ timeshift deja-dup flatpak ffmpegthumbnailer curl dkms linux-headers-amd64
 
-# 8. Activate flatpak
+# 9. Activate Flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# 9. Clone Tela-icon-theme repo and run install.sh
-git clone https://github.com/vinceliuice/Tela-icon-theme.git /home/mike/Git/Tela-icon-theme
-chown -R mike:mike /home/mike/Git/Tela-icon-theme
-sudo -u mike bash -c "cd /home/mike/Git/Tela-icon-theme && ./install.sh"
+# 10. Clone Tela-icon-theme repo
+sudo -u mike git clone https://github.com/vinceliuice/Tela-icon-theme.git /home/mike/Git/Tela-icon-theme
 
-# 10. Clone Orchis-theme repo and run install.sh
-git clone https://github.com/vinceliuice/Orchis-theme.git /home/mike/Git/Orchis-theme
-chown -R mike:mike /home/mike/Git/Orchis-theme
-sudo -u mike bash -c "cd /home/mike/Git/Orchis-theme && ./install.sh"
+# 11. Run install.sh in Tela-icon-theme
+sudo -u mike bash /home/mike/Git/Tela-icon-theme/install.sh
 
-# 11. Clone Qogir-icon-theme repo and run install.sh
-git clone https://github.com/vinceliuice/Qogir-icon-theme.git /home/mike/Git/Qogir-icon-theme
-chown -R mike:mike /home/mike/Git/Qogir-icon-theme
-sudo -u mike bash -c "cd /home/mike/Git/Qogir-icon-theme && ./install.sh"
+# 12. Clone Orchis-theme repo
+sudo -u mike git clone https://github.com/vinceliuice/Orchis-theme.git /home/mike/Git/Orchis-theme
 
-# 12. Install Fisher
+# 13. Run install.sh in Orchis-theme
+sudo -u mike bash /home/mike/Git/Orchis-theme/install.sh
+
+# 14. Clone Qogir-icon-theme repo
+sudo -u mike git clone https://github.com/vinceliuice/Qogir-icon-theme.git /home/mike/Git/Qogir-icon-theme
+
+# 15. Run install.sh in Qogir-icon-theme
+sudo -u mike bash /home/mike/Git/Qogir-icon-theme/install.sh
+
+# 16. Install Fisher
 sudo -u mike bash -c "curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher"
 
-# 13. Install bobthefish theme
-sudo -u mike bash -c "fish -c 'fisher install oh-my-fish/theme-bobthefish'"
+# 17. Install bobthefish theme
+# To run fish commands, ensure fish shell is installed and set as default or run via fish -c
+apt install -y fish
+sudo -u mike fish -c "fisher install oh-my-fish/theme-bobthefish"
 
-echo "Stable setup completed successfully."
+echo "Setup completed successfully!"
