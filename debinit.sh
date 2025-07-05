d27ea79b47a7d119447cbed1b4fbb08104220b83
mike <mike@debian>
2025-07-05 11:41:28 -0500

    update debinit.sh

--- debinit.sh
+++ debinit.sh
@@ -15,14 +15,3 @@ usermod -aG sudo mike
 
 # Step 3: Install git
 apt install -y git
-
-# Step 4: Create directory /home/mike/Git
-mkdir -p /home/mike/Git
-chown mike:mike /home/mike/Git
-
-# Step 5: Clone your main repo
-sudo -u mike git clone https://github.com/C01NoP/debian-config.git /home/mike/Git/debian-config
-
-# Step 6: Make stablesetup.sh executable and run it
-sudo -u mike chmod +x /home/mike/Git/debian-config/stablesetup.sh
-sudo -u mike /home/mike/Git/debian-config/stablesetup.sh
