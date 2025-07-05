26c4b41f855de54a224743e08f656b7c26d37c1e
mike <mike@debian>
2025-07-05 12:42:44 -0500

    update stablesetup.sh script

--- stablesetup.sh
+++ stablesetup.sh
@@ -1,27 +1,51 @@
+
 #!/bin/bash
+set -e
 
-# Update package list
+# 7. Install packages
 apt update
+apt install -y gnome-shell gnome-tweaks nautilus vim fish firefox kitty vlc crispy-doom dsda-doom gnome-calculator timeshift deja-dup flatpak ffmpegthumbnailer curl wget dkms linux-headers-amd64
 
-# Step 7: Install packages
-apt install -y gnome-shell gnome-tweaks nautilus vim fish firefox kitty vlc crispy-doom dsda-doom gnome-calculator timeshift deja-dup flatpak ffmpegthumbnailer vlc
-
-# Step 8: Activate Flatpak
+# 8. Activate flatpak
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# Step 9: Clone Tela-icon-theme
-git clone https://github.com/vinceliuice/Tela-icon-theme /home/mike/Git/Tela-icon-theme
-# Run its install script
-bash /home/mike/Git/Tela-icon-theme/install.sh
+# 9-14. Clone themes and run their install.sh
+mkdir -p "$HOME_DIR/Git"
 
-# Step 10: Clone Orchis-theme
-git clone https://github.com/vinceliuice/Orchis-theme /home/mike/Git/Orchis-theme
-bash /home/mike/Git/Orchis-theme/install.sh
+git clone https://github.com/vinceliuice/Tela-icon-theme.git "$HOME_DIR/Git/Tela-icon-theme"
+(cd "$HOME_DIR/Git/Tela-icon-theme" && ./install.sh)
 
-# Step 11: Clone Qogir-icon-theme
-git clone https://github.com/vinceliuice/Qogir-icon-theme /home/mike/Git/Qogir-icon-theme
-bash /home/mike/Git/Qogir-icon-theme/install.sh
+git clone https://github.com/vinceliuice/Orchis-theme.git "$HOME_DIR/Git/Orchis-theme"
+(cd "$HOME_DIR/Git/Orchis-theme" && ./install.sh)
 
-# Reboot after all operations
-echo "All setup complete. Rebooting now..."
+git clone https://github.com/vinceliuice/Qogir-icon-theme.git "$HOME_DIR/Git/Qogir-icon-theme"
+(cd "$HOME_DIR/Git/Qogir-icon-theme" && ./install.sh)
+
+# 15. Move fish.config
+if [ -f "$REPO_PATH/debian-config/fish.config" ]; then
+    mkdir -p "$HOME_DIR/.config/fish"
+    cp "$REPO_PATH/debian-config/fish.config" "$HOME_DIR/.config/fish/config.fish"
+fi
+
+# 16. Move kitty.config
+if [ -f "$REPO_PATH/debian-config/kitty.config" ]; then
+    mkdir -p "$HOME_DIR/.config/kitty"
+    cp "$REPO_PATH/debian-config/kitty.config" "$HOME_DIR/.config/kitty/"
+fi
+
+# 17. Copy entire /root/Git to /home/mike/Git
+# First, ensure /home/mike/Git exists
+mkdir -p "$GIT_DIR"
+# Remove existing to prevent conflicts
+rm -rf "$GIT_DIR"
+# Copy recursively
+cp -r "$ROOT_HOME/Git" "$GIT_DIR"
+
+# Change ownership to user
+chown -R "$USER":"$USER" "$HOME_DIR"
+
+# 18. Reboot
+echo "Setup complete. Rebooting now..."
 reboot
+
+# --- End of debian-config/stablesetup.sh ---
